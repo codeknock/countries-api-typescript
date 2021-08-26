218a9b67c9b7788e2a08f2219b52729931191f2a
chandrakant nyaupane <chandra@MacBook-Pro.local>
2021-08-26 20:56:16 +0300

    redux boileplate created, data dispatched throught redux thunk

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,27 @@ export type Country = {
   }
 
   export type Languages = {name: string, nativeName: string}
+
+  //Fetching countries
+
+  export const FETCH_COUNTRIES = "FETCH_COUNTRIES"
+  export const FETCH_COUNTRIES_SUCCESS = "FETCH_COUNTRIES_SUCCESS"
+  export const FETCH_COUNTRIES_FAILURE = "FETCH_COUNTRIES_FAILURE"
+
+  //ALL actions
+  
+
+export type FetchCountriesAction = {
+    type: typeof FETCH_COUNTRIES
+   
+}
+export type FetchCountriesSuccessAction = {
+    type: typeof FETCH_COUNTRIES_SUCCESS
+   payload: [],
+}
+export type FetchCountriesFailureAction = {
+    type: typeof FETCH_COUNTRIES_FAILURE
+   payload: string,
+}
+
+export type AllActions = FetchCountriesAction | FetchCountriesSuccessAction | FetchCountriesFailureAction
